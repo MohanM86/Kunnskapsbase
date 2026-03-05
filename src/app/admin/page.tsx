aef0dd3ad05c166bff27c9a985c660822da00608
MohanM86 <mohanmanoharan86@gmail.com>
2026-03-05 20:11:47 +0100

    slett

--- src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AdminPage() {
-  return (
-    <div className="admin-page">
-      <h1>Admin</h1>
-      <p>Decap CMS kjører som en statisk side. Gå til <a href="/admin/index.html">/admin/index.html</a> for å logge inn.</p>
-    </div>
-  );
-}
